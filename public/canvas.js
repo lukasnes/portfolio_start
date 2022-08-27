043202babbcda70721c84d14fb73a9d7a8033436
unknown <lukasnesittka25@gmail.com>
2022-08-27 12:05:20 -0500

    Goober Canvas implemented

--- public/canvas.js
+++ public/canvas.js
@@ -5,106 +5,92 @@ canvas.height = window.innerHeight
 
 let context = canvas.getContext('2d')
 
-//create rectangles
-// context.fillStyle = "rgba(255,0,0,0.3)"
-// context.fillRect(100,100,100,100)
-// context.fillStyle = "rgba(0,255,0,0.3)"
-// context.fillRect(300,600,100,100)
-
-// //create lines
-// context.beginPath()
-// context.moveTo(50,300)
-// context.lineTo(300,100)
-// context.lineTo(600,800)
-// context.lineTo(1200,400)
-// context.strokeStyle = "red"
-// context.stroke()
-
-// //create circles/arcs
-// for(i = 0; i < 50; i++){
-//     let randomX = Math.random() * window.innerWidth
-//     let randomY = Math.random() * window.innerHeight
-
-//     context.beginPath()
-//     context.arc(randomX,randomY,50,0,Math.PI * 2,false)
-//     context.strokeStyle = "purple"
-//     context.stroke()
-// }
+//Getting the goober images
+let goobers = [
+    './goobers/crabman.png',
+    './goobers/cyclops.png',
+    './goobers/flower.png',
+    './goobers/lion.png',
+    './goobers/lizardman.png',
+    './goobers/monk.png',
+    './goobers/monke.png',
+    './goobers/turtle.png'
+]
+//Defining a starting mouse position
 let mouse = {
     x: undefined,
     y: undefined
 }
+//Creating a reusable class for storing Goober images
+class Goober {
+    constructor(image,x,y,xVelocity,yVelocity){
+        this.image = image
 
-
-class Circle {
-    static count = 0
-    constructor(x,y,xVelocity,yVelocity,radius) {
-        this.xPosition = x
-        this.yPosition = y
+        this.xPosition = x;
+        this.yPosition = y;
         
-        this.xVelocity = xVelocity
-        this.yVelocity = yVelocity
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
         
-        this.radius = radius
+        this.widthScale = 1;
+        this.heightScale = 1;
     }
-
+    //Defining how the goobers will be drawn on screen
     draw = () => {
-        context.beginPath()
-        context.arc(this.xPosition,this.yPosition,this.radius,0,Math.PI * 2,false)
-        context.strokeStyle = "blue"
-        context.stroke()
-        context.fillStyle = "aquamarine"
-        context.fill()
+        context.drawImage(this.image,this.xPosition,this.yPosition,this.image.width * this.widthScale, this.image.height * this.heightScale)
     }
+    //Creating a function that shrinks the goober image
+    shrink = () => {
+        //Conditional for making sure the scale doesn't revert to negative values
+        if(this.widthScale <= 0 || this.heightScale <= 0) {
+            return
+        }
 
+        this.widthScale -= 0.01
+        this.heightScale -= 0.01
+    }
+    //Redrawing the images every frame of their existence in the list, moving them, and making sure they bounce off the walls of the screen
     update = () => {
-        if(this.xPosition + this.radius > innerWidth || this.xPosition - this.radius < 0) {
+        if(this.xPosition + this.image.width > innerWidth || this.xPosition - this.image.width < 0) {
             this.xVelocity *= -1
         }
-        if(this.yPosition + this.radius > innerHeight || this.yPosition - this.radius < 0) {
+        if(this.yPosition + this.image.height > innerHeight || this.yPosition - this.image.height < 0) {
             this.yVelocity *= -1
         }
 
         this.xPosition += this.xVelocity
         this.yPosition += this.yVelocity
 
-        if(mouse.x - this.xPosition < 50 && mouse.x - this.xPosition > -50 && mouse.y - this.yPosition < 50 && mouse.y - this.yPosition > -50 && this.radius <= 100) {
-            this.radius += 3
-        } else {
-            if(this.radius > 5) {
-                this.radius -= 1
-            }
-        }
-
         this.draw()
+        this.shrink()
     }
 }
-
-class CircleNode {
+//Creating nodes for the Goobers to sit in
+class GooberNode {
     constructor(value) {
         this.value = value;
         this.next = null;
-        this.previous = null
+        this.previous = null;
     }
 }
-
-class DoubleLinkedCircles {
+//Doubly-linked list for the Goober Nodes
+class GooberLinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
     }
 
     addToBack(value) {
-        let newCircle = value;
+        let newGoober = value;
         if(this.head === null) {
-            this.head = newCircle
-            this.tail = newCircle
+            this.head = newGoober;
+            this.tail = newGoober;
         } else {
-            this.tail.next = newCircle
-            newCircle.previous = this.tail
-            this.tail = newCircle
+            this.tail.next = newGoober;
+            newGoober.previous = this.tail;
+            this.tail = newGoober;
         }
-        return `Added new node ${newCircle} to Circle List.`
+        return `Added new Goober ${newGoober} to Goober List.`
     }
 
     size() {
@@ -124,26 +110,27 @@ class DoubleLinkedCircles {
 
         if(this.head === this.tail) {
             let temp = this.head
+            while(this.head.value.image.width > 0 && this.head.value.image.height > 0) {
+                this.head.value.shrink()
+            }
             this.head = null;
             this.tail = null;
             return temp.value
         }
-        for(i = 0; i < 10; i++){
-            let temp = this.head;
-            this.head = this.head.next
-            this.next = null
-            temp.next = null
-        }
+        let temp = this.head;
+        this.head = this.head.next;
+        this.next = null
+        temp.next = null;
     }
-
-    drawCircles(runner = this.head) {
-        if(runner === null){
+    //This function is where the Goober update method actually gets called
+    drawGoobers(runner = this.head) {
+        if(runner === null) {
             return
         }
 
         if(runner.next) {
             runner.value.update()
-            return this.drawCircles(runner.next)
+            return this.drawGoobers(runner.next)
         } else {
             runner.value.update()
             return
@@ -151,47 +138,54 @@ class DoubleLinkedCircles {
     }
 }
 
-let circleList = new DoubleLinkedCircles()
+let gooberList = new GooberLinkedList()
 
-for(i = 0; i < 100; i++){
-    let radius = 5
-    let xPosition = (Math.random() * (innerWidth - radius * 2) + radius)
-    let yPosition = (Math.random() * (innerHeight - radius * 2) + radius)
+//Creating fresh goobers regularly
+const freshGoobers = () => {
+    let image = new Image()
+    image.src = goobers[Math.floor(Math.random() * goobers.length)]
+    let xPosition = (Math.random() * (innerWidth - image.width) + image.width)
+    let yPosition = (Math.random() * (innerHeight - image.height) + image.height)
     let xVelocity = ((Math.random() - 0.5) * 10)
     let yVelocity = ((Math.random() - 0.5) * 10)
     
-    console.log(circleList)
-    circleList.addToBack(new CircleNode(new Circle(xPosition,yPosition,xVelocity,yVelocity,radius)))
+    gooberList.addToBack(new GooberNode(new Goober(image,xPosition,yPosition,xVelocity,yVelocity)))
 }
 
+//Creating an animate function to be called every frame
 const animate = () => {
     requestAnimationFrame(animate)
     context.clearRect(0,0,innerWidth,innerHeight)
     
-    circleList.drawCircles()
+    if(gooberList.size() < 550) {
+        freshGoobers()
+    }
+    console.log(gooberList.size())
+    gooberList.drawGoobers()
     removeExcess()
 }
 
-
-const mouseCircles = (evt) => {
+//Setting the mouse position and creating new goobers on mouse move
+const mouseGoobers = (evt) => {
     mouse.x = evt.x
     mouse.y = evt.y
-    console.log(circleList.size())
 
-    for(i = 0; i < 5; i++){
-        let xVelocity = ((Math.random() - 0.5) * 10)
-        let yVelocity = ((Math.random() - 0.5) * 10)
-        let radius = 5
-        circleList.addToBack(new CircleNode(new Circle(mouse.x,mouse.y,xVelocity,yVelocity,radius)))
-    }
+    let image = new Image()
+    image.src = goobers[Math.floor(Math.random() * goobers.length)]
+    let xPosition = mouse.x
+    let yPosition = mouse.y
+    let xVelocity = ((Math.random() - 0.5) * 10)
+    let yVelocity = ((Math.random() - 0.5) * 10)
+
+    gooberList.addToBack(new GooberNode(new Goober(image,xPosition,yPosition,xVelocity,yVelocity)))
 }
-
+//Removing excess goobers to reduce CPU strain
 const removeExcess = () => {
-    if(circleList.size() >= 1000) {
-        circleList.removeFront()
+    while(gooberList.size() >= 500) {
+        gooberList.removeFront()
     }
 }
 
-canvas.addEventListener('mousemove', mouseCircles)
+canvas.addEventListener('mousemove', mouseGoobers)
 console.log(mouse)
 animate()
